006ee2ffcdfce2172640f0eed9697882ed585a80
Joe Allen <allen.joeg@gmail.com>
2023-05-05 10:33:19 -0500

    TarotController returning all or by id. Migration reset with schema tweaks again (expect another round).

--- API Platform Library/Controllers/Tarot/TarotController.cs
+++ API Platform Library/Controllers/Tarot/TarotController.cs
@@ -1,21 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API_Platform_Library.DAL;
+using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
 namespace API_Platform_Library.Controllers.Tarot
 {
     [ApiController]
-    [Route("[tarot]")]
+    [Route("[controller]")]
     public class TarotController : ControllerBase
     {
+        public readonly ApiPlatformContext _context;
+        public TarotController(ApiPlatformContext context)
+        {
+            _context = context;
+        }
 
 
-        //[HttpGet(Name = "GetTarotCard")]
-        //public async Task<ActionResult> Get()
-        //{
+        [HttpGet]
+        public IEnumerable<Models.Tarot.Tarot> GetAll()
+        {
+            return _context.Tarot;
+        }
 
-        //    return;
+        [HttpGet("{id}")]
+        public ActionResult<Models.Tarot.Tarot> GetById(int id)
+        {
+            Models.Tarot.Tarot card = _context.Tarot
+                .SingleOrDefault(card => card.ID == id);
 
-        //}
+            if(card is null)
+            {
+                return NotFound();
+            }
+
+            return card;
+
+        }
     }
 
 }
