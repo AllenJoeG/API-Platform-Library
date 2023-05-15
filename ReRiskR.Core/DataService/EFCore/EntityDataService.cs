b638e8d5cce12dd47ac305321caa2c17f1b8d9cd
Joe Allen <allen.joeg@gmail.com>
2023-05-15 17:48:11 -0500

    Implementation of Data Service Interface with DbContext code, and Tasks GetAll() and GetById().

---
+++ ReRiskR.Core/DataService/EFCore/EntityDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReRiskR.Core.DataService.EFCore
+{
+    internal class EntityDataService
+    {
+    }
+}
