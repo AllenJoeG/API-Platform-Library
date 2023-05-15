4de99fb7b034413e2a1eb7a707b46ce433601066
Joe Allen <allen.joeg@gmail.com>
2023-05-15 17:40:42 -0500

    New Core RRR. Interface for basic CRUD Data Service created.

---
+++ ReRiskR.Core/DataService/IEntityDataService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReRiskR.Core.DataService
+{
+    public interface IEntityDataService<TEntity> where TEntity : class, new()
+    {
+        Task<TEntity> GetById<TId>(TId id);
+
+        Task<IList<TEntity>> GetAll();
+
+        Task<TEntity> Add(TEntity entity);
+
+        Task<TEntity> Update(TEntity entity);
+
+        Task Delete(TEntity entity)
+    }
+}
