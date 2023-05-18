4aeae2b44e12b8313dde6c4d358589e803a6e9d0
Joe Allen <allen.joeg@gmail.com>
2023-05-17 21:59:13 -0500

    EntityDataFactory and extension of such for EntityDomainDataFactory. Leverage AutoFixture library to generate valid TEntity for Tests.

---
+++ ReRiskR.Test.Core/DataGeneration/DataFactories/EntityDataFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AutoFixture;
+using AutoFixture.DataAnnotations;
+
+namespace ReRiskR.Test.Core.DataGeneration.DataFactories
+{
+    public class EntityDataFactory<TEntity>
+    {
+        public virtual TEntity Factory_Entity(Action<TEntity> action = null)
+        {
+            var fixture = Factory_Fixture();
+
+            var obj = fixture.Create<TEntity>();
+
+            action?.Invoke(obj);
+
+            return obj;
+        }
+
+
+        //Plenty to learn about here from AutoFixture documentation and EISK implementation
+        private Fixture Factory_Fixture()
+        {
+            Fixture fixture = new Fixture();
+
+            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            fixture.Customize(new NoDataAnnotationsCustomization());
+            fixture.Customizations.Add(new RangeAttributeRelay());
+
+            //This is an EISK custom class to be imported
+            //fixture.Customizations.Add(new StringAttributeAggregatedRelay());
+            return fixture;
+        }
+    }
+}
---
+++ ReRiskR.Test.Core/DataGeneration/DataFactories/EntityDomainDataFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReRiskR.Test.Core.DataGeneration.DataFactories
+{
+    public abstract class EntityDomainDataFactory<TEntity> : EntityDataFactory<TEntity>
+    {
+        protected abstract void AssignDomainData(TEntity e);
+
+        public override TEntity Factory_Entity(Action<TEntity> action = null)
+        {
+            var entity = base.Factory_Entity(e =>
+            {
+                AssignDomainData(e);
+
+                //EISK documentation suggests this supports custom
+                //overrides from user. I do not understand!
+                action?.Invoke(e);
+            });
+
+            return entity;
+        }
+    }
+}
