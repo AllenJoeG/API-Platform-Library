0984f40f27eb0a7d3ce8c94ee93eda46a4e0a239
Joe Allen <allen.joeg@gmail.com>
2023-05-17 21:38:23 -0500

    Started Test Project for Core modeling off of EISK's work. Basic random String and Integer value generators.

---
+++ ReRiskR.Test.Core/DataGeneration/FieldGenerators/IntegerValueGenerator.cs
@@ -0,0 +1,22 @@
+﻿//Straightforward random Int32 number generator for testing
+
+namespace ReRiskR.Test.Core.DataGeneration.FieldGenerators
+{
+    public class IntegerValueGenerator
+    {
+        private readonly int _maxValue;
+
+        public IntegerValueGenerator(int maxValue = Int32.MaxValue)
+        {
+            _maxValue = maxValue;
+        }
+
+        public int Value()
+        {
+            return new Random().Next(1, _maxValue);
+        }
+
+        public static int RandomInt => 
+            new IntegerValueGenerator().Value();
+    }
+}
---
+++ ReRiskR.Test.Core/DataGeneration/FieldGenerators/StringValueGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace ReRiskR.Test.Core.DataGeneration.FieldGenerators
+{
+    public class StringValueGenerator
+    {
+        private readonly string _seedValue;
+        private readonly bool _generateUniqueValue;
+
+        public StringValueGenerator(bool generateUniqueValue, string seedValue)
+        {
+            _generateUniqueValue = generateUniqueValue;
+            _seedValue = seedValue;
+        }
+
+        //If Unique bool flagged, attach unique GUID to provided seedValue
+        public override string ToString()
+        {
+            return !_generateUniqueValue ?
+                _seedValue : $"{_seedValue}_{Guid.NewGuid()}";
+        }
+    }
+}
